89b218d2acbbcdb3d5a019326a3f201b1eb1eb76
Souptik Bej <bejsouptik@gmail.com>
2023-02-17 23:59:44 +0530

    2nd commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let menu=document.getElementById('menu-icon');
+let crosse=document.getElementById('crose');
+let slideber=document.getElementById('slideber');
+function slide(){
+    console.log('hello');
+    slideber.style.display='block';
+}
+function crose(){
+    console.log('hello');
+    slideber.style.display='none';
+}
