02c071a80755f6236b5e6fcb43a9aba777052676
Souptik Bej <bejsouptik@gmail.com>
2023-02-20 23:42:01 +0530

    some change

--- script.js
+++ script.js
@@ -34,27 +34,53 @@ function crose() {
 //     }
 //     slide[b].style.display = "block";
 // }
-let flag = 0;
-function controller(x) {
-    flag = flag + x;
-    slideshow(flag);
+// let flag = 0;
+// function controller(x) {
+//     flag = flag + x;
+//     slideshow(flag);
+// }
+
+// slideshow(flag);
+// function slideshow(num) {
+//     let slides = document.getElementsByClassName('slide');
+//     if (num == slides.length) {
+//         flag = 0;
+//         num = 0;
+//     }
+//     if (num < 0) {
+//         flag = slides.length - 1;
+//         num = slides.length - 1;
+//     }
+//     for (let x of slides) {
+//         x.style.display = "none";
+
+//     }
+//     slides[num].style.display = "block";
+
+// }
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
 }
 
-slideshow(flag);
-function slideshow(num) {
-    let slides = document.getElementsByClassName('slide');
-    if (num == slides.length) {
-        flag = 0;
-        num = 0;
-    }
-    if (num < 0) {
-        flag = slides.length - 1;
-        num = slides.length - 1;
-    }
-    for (let x of slides) {
-        x.style.display = "none";
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
 
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
     }
-    slides[num].style.display = "block";
-
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
 }
